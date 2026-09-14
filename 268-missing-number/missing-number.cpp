51cb6f8aad5c2b442c35089c3dbbbecc95f95340
shigi-2003 <shivangisel222@gmail.com>
2026-09-14 13:54:46 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (70.93%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = n*(n+1)/2;
+
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum = sum + nums[i];
+        }
+
+        int ans = totalSum - sum;
+        return ans;
+    }
+};
